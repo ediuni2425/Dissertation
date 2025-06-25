450b509bed9781c3d3ba2dec975c23e4eb5435c0
Kathleen Owen <kathleenowen@MacBook-Air-479.local>
2025-06-25 20:03:57 +0100

    preparing data and packages for analysis

--- Dissertation.R
+++ Dissertation.R
@@ -1,2 +1,17 @@
 #Chlorophyll fluorescence trial analysis 
 
+#Loading data 
+cftrial <- read.csv("~/Desktop/MSc EEB/WD/Dissertation/CFtrial.csv")
+
+#Installing + loading packages
+install.packages("lme4")
+install.packages("lmerTest")
+
+library(lme4)
+library(lmerTest)
+
+#Convert categorical variables to factors
+cftrial$plot <- as.factor(cftrial$plot)
+cftrial$time <- as.factor(cftrial$time)
+cftrial$plant <- as.factor(cftrial$plant)
+cftrial$leaf <- as.factor(cftrial$leaf)
